761c39589c3c2282633aad5a7a9d2b5fcd2622ea
patataofcourse <patataofcourse@gmail.com>
2022-03-09 01:57:26 +0100

    header stuff

--- src/lib.rs
+++ src/lib.rs
@@ -2,4 +2,11 @@ mod etc_dec;
 
 pub struct EffectFile {
     pub version: u32,
+    pub unk: u64,
+    pub emitter_sets: Vec<EmitterSet>,
+    pub texture_folder: Vec<Texture>,
 }
+
+pub struct EmitterSet {}
+
+pub struct Texture {}
